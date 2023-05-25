85031f171106eba17a4a99edd7aefa81c9a9970b
csk6314 <37896060+csk6314@users.noreply.github.com>
2023-05-25 17:49:14 +0900

    [develop] fix 1

--- src/components/layout/sidemenu/index.tsx
+++ src/components/layout/sidemenu/index.tsx
@@ -2,19 +2,32 @@ import { useState } from "react";
 import SideHeader from "./components/SideHeader";
 import SideLoginSection from "./components/SideLoginSection";
 import SideMenuSection from "./components/SideMenuSection";
+import { ReactComponent as Logo } from "../../../assets/Logo.svg";
+import { Link } from "react-router-dom";
 
 const SideMenu = () => {
   const [isOpen, setIsOpen] = useState<boolean>(true);
   return (
-    <div
-      className={`${
-        isOpen ? "w-[300px]" : "w-[70px]"
-      } h-[100%] bg-primary flex-col transition-[width] duration-300 ease-in-out`}
-    >
-      <SideHeader isOpen={isOpen} setIsOpen={setIsOpen} />
-      <SideLoginSection isOpen={isOpen} />
-      <SideMenuSection isOpen={isOpen} />
-    </div>
+    <>
+      <div
+        className={`${
+          isOpen ? "w-[300px]" : "w-[70px]"
+        }   hidden md:block h-[100%] bg-primary flex-col transition-[width] duration-300 ease-in-out`}
+      >
+        <SideHeader isOpen={isOpen} setIsOpen={setIsOpen} />
+        <SideLoginSection isOpen={isOpen} />
+        <SideMenuSection isOpen={isOpen} />
+      </div>
+      <div
+        className={`w-[70px] md:hidden h-[100%] bg-primary flex-col transition-[width] duration-300 ease-in-out`}
+      >
+        <Link to="/" className="flex justify-center">
+          <div className="py-[10px] text-white text-lg font-bold">HOME</div>
+        </Link>
+        <SideLoginSection isOpen={false} />
+        <SideMenuSection isOpen={false} />
+      </div>
+    </>
   );
 };
 
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Layout from "../components/layout";
+import MainPage from "../pages/main";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<div>Swing</div>} />
+          <Route path="/" element={<MainPage />} />
           <Route path="/login" element={<div>Login</div>} />
           <Route path="/publish" element={<div>Publish</div>} />
           <Route path="/transactions" element={<div>transactions</div>} />
