a0e1a5d457ac9d31418b8f208fd9081babc252af
csk6314 <37896060+csk6314@users.noreply.github.com>
2023-05-26 20:14:26 +0900

    [develop] utils setting

---
+++ src/util/util.ts
@@ -0,0 +1,15 @@
+type priceFormatT = (price: number) => string;
+export const priceFormat: priceFormatT = (price) => {
+  return price.toString().replace(/\B(?=(\d{3})+(?!\d))/, ",");
+};
+
+type dateFormatT1 = (date: Date) => string;
+export const dateFormat: dateFormatT1 = (date) => {
+  return (
+    date.getFullYear() +
+    "-" +
+    (date.getMonth() + 1).toString().padStart(2, "0") +
+    "-" +
+    date.getDate().toString().padStart(2, "0")
+  );
+};
